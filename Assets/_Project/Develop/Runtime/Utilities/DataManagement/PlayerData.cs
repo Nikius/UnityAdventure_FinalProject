7d420a9bac1281fbc3959366e817d310d33efd8c
Nikius-PC\Nikius <mikhail.dudek@gmail.com>
2025-07-12 18:45:22 +0300

    install NuGet package

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _Project.Develop.Runtime.Meta.Features.Wallet;
+
+namespace _Project.Develop.Runtime.Utilities.DataManagement
+{
+    public class PlayerData
+    {
+        public Dictionary<CurrencyTypes, int> WalletData;
+        
+        
+    }
+}
