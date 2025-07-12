91d6f1487b21885016afd4c541b1024fd97ab1e3
Nikius-PC\Nikius <mikhail.dudek@gmail.com>
2025-07-12 18:28:10 +0300

    add WalletService

--- Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuBootstrap.cs
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuBootstrap.cs
@@ -2,6 +2,7 @@
 using _Project.Develop.Runtime.Gameplay.Infrastructure;
 using _Project.Develop.Runtime.Infrastructure;
 using _Project.Develop.Runtime.Infrastructure.DI;
+using _Project.Develop.Runtime.Meta.Features.Wallet;
 using _Project.Develop.Runtime.Utilities.CoroutinesManagement;
 using _Project.Develop.Runtime.Utilities.SceneManagement;
 using UnityEngine;
@@ -11,6 +12,8 @@ namespace _Project.Develop.Runtime.Meta.Infrastructure
     public class MainMenuBootstrap: SceneBootstrap
     {
         private DIContainer _container;
+        
+        private WalletService _walletService;
 
         public override void ProcessRegistrations(DIContainer container, IInputSceneArgs sceneArgs = null)
         {
@@ -22,6 +25,8 @@ namespace _Project.Develop.Runtime.Meta.Infrastructure
         public override IEnumerator Initialize()
         {
             Debug.Log("MainMenuBootstrap initialized");
+            
+            _walletService = _container.Resolve<WalletService>();
             
             yield break;
         }
@@ -43,6 +48,21 @@ namespace _Project.Develop.Runtime.Meta.Infrastructure
                 SceneSwitcherService sceneSwitcherService = _container.Resolve<SceneSwitcherService>();
                 ICoroutinesPerformer coroutinesPerformer = _container.Resolve<ICoroutinesPerformer>();
                 coroutinesPerformer.StartPerform(sceneSwitcherService.ProcessSwitchTo(Scenes.Gameplay, new GameplayInputArgs(2)));
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                _walletService.Add(CurrencyTypes.Gold, 10);
+                Debug.Log("Gold: " + _walletService.GetCurrency(CurrencyTypes.Gold).Value);
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                if (_walletService.Enough(CurrencyTypes.Gold, 10) == false)
+                    return;
+                
+                _walletService.Spend(CurrencyTypes.Gold, 10);
+                Debug.Log("Gold: " + _walletService.GetCurrency(CurrencyTypes.Gold).Value);
             }
         }
     }
