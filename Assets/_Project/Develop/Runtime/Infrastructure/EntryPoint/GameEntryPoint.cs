a32b02471fa43f642afc4bca5b62d335482debca
Nikius-PC\Nikius <mikhail.dudek@gmail.com>
2025-07-12 19:09:30 +0300

    optimize imports

--- Assets/_Project/Develop/Runtime/Infrastructure/EntryPoint/GameEntryPoint.cs
+++ Assets/_Project/Develop/Runtime/Infrastructure/EntryPoint/GameEntryPoint.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using _Project.Develop.Runtime.Infrastructure.DI;
 using _Project.Develop.Runtime.Utilities.ConfigsManagement;
 using _Project.Develop.Runtime.Utilities.CoroutinesManagement;
